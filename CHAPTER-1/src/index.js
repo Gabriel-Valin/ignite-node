b02117d6e7237219795a01a73b2ce9316ed893dd
Gabriel-Valin <gabrielvalincontato@gmail.com>
2021-11-01 22:49:00 -0300

    feat: create a new account and validate CPF

--- CHAPTER-1/src/index.js
+++ CHAPTER-1/src/index.js
@@ -1,7 +1,31 @@
 const express = require('express')
+const crypto = require('crypto')
 
 const app = express()
+const accounts = []
 
 app.use(express.json())
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body
+    const foundCpf = accounts.some(account => account.cpf === cpf)
+
+    if (foundCpf) {
+        return response.status(400).json({ error: 'CPF already registered!' })
+    }
+
+    const id = crypto.randomUUID
+
+    accounts.push({ id, cpf, name, statement: [] })
+
+    return response.sendStatus(201)
+})
+
+app.get('/statement/:cpf', (request, response) => {
+    const { cpf } = request.params
+    const account = accounts.find(account => account.cpf === cpf)
+
+    return response.json(account.statement)
+})
 
 app.listen(3331, () => console.log('Server running at PORT: 3331 =)'))
